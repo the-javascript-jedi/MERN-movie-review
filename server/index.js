3c1354e39727d4a33b2e50aeb0505efa172646a5
Nithin Samuel <nithin.thewebdev@gmail.com>
2022-02-21 17:26:33 +0530

    added mySql queries

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
+// const mysql = require("mysql");
+//due to security issue using mysql2
+const mysql = require("mysql2");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "mysqlroot",
+  database: "cruddatabase",
+});
+
 // http://localhost:5000/
 app.get("/", (req, res) => {
-  res.send("Hello Nithin");
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movieName,movieReview) VALUES ('inception','good movie')";
+  db.query(sqlInsert, (err, result) => {
+    console.log("err", err);
+    console.log("result", result);
+    res.send("Hello Nithin");
+  });
 });
 app.listen(5000, () => {
   console.log("running on port 5000");
